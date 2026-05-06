aa8830aeba062df58cfe6c1985d258264e6a927b
Nemoto Mailon <minatokiroisen77@gmail.com>
2026-05-06 22:24:15 +0900

    setup middleware on errorHanlder

--- subscription-tracker/app.js
+++ subscription-tracker/app.js
@@ -1,20 +1,29 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import connectToDatabase from './database/mongodb.js'
-
-import { PORT } from './config/env.js'
+import cookieParser from 'cookie-parser'
 
 import userRouter from './routes/user.routes.js'
 import authRouter from './routes/auth.routes.js'
 import subscriptionRouter from './routes/subscription.routes.js'
 
+import errorHandlerMiddleware from './middleware/errorHandler.middleware.js'
+
+import { PORT } from './config/env.js'
+
 const app = express()
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+
 dotenv.config()
 connectToDatabase()
 
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/users', userRouter)
 app.use('/api/v1/subscription', subscriptionRouter)
+
+app.use(errorHandlerMiddleware)
 
 app.get("/", (req, res) => {
     res.send("Welcome to the Subs-tracker backend production ready.")
